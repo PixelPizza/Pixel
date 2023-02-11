b44e18aa53a02b4171f5e33680c9b65646157426
Jaron <jaron_zijlstra@outlook.com>
2023-02-11 22:57:16 +0100

    chore(Builders): add more tests

--- packages/builders/test/slashcommand.test.ts
+++ packages/builders/test/slashcommand.test.ts
@@ -7,85 +7,219 @@ import {
 	SlashCommandSubcommandGroupBuilder
 } from "../src";
 
-const createInvalidBuilder = () => new SlashCommandBuilder("", "");
-const createValidBuilder = () => new SlashCommandBuilder("test", "test description");
-const createValidBuilderWithOptionalParams = () =>
-	new SlashCommandBuilder("test", "test description", {
-		nameLocalizations: {
-			de: "test",
-			"en-GB": "test"
-		},
-		descriptionLocalizations: {
-			de: "test description",
-			"en-GB": "test description"
-		},
-		dmPermission: true,
-		defaultMemberPermissions: 8
-	});
-const createValidBuilderWithOptions = () =>
-	createValidBuilder()
-		.addOption(
-			new SlashCommandOptionBuilder("attachment", ApplicationCommandOptionType.Attachment, "attachment option")
-		)
-		.addOption(new SlashCommandOptionBuilder("boolean", ApplicationCommandOptionType.Boolean, "boolean option"))
-		.addOption(new SlashCommandOptionBuilder("channel", ApplicationCommandOptionType.Channel, "channel option"))
-		.addOption(new SlashCommandOptionBuilder("integer", ApplicationCommandOptionType.Integer, "integer option"))
-		.addOption(
-			new SlashCommandOptionBuilder("mentionable", ApplicationCommandOptionType.Mentionable, "mentionable option")
-		)
-		.addOption(new SlashCommandOptionBuilder("number", ApplicationCommandOptionType.Number, "number option"))
-		.addOption(new SlashCommandOptionBuilder("role", ApplicationCommandOptionType.Role, "role option"))
-		.addOption(new SlashCommandOptionBuilder("string", ApplicationCommandOptionType.String, "string option"))
-		.addOption(new SlashCommandOptionBuilder("user", ApplicationCommandOptionType.User, "user option"));
-const createValidBuilderWithSubcommand = () =>
-	createValidBuilder().addSubcommand(new SlashCommandSubcommandBuilder("subcommand", "subcommand description"));
-const createValidBuilderWithSubcommandGroup = () =>
-	createValidBuilder().addSubcommandGroup(
-		new SlashCommandSubcommandGroupBuilder("subcommandgroup", "subcommandgroup description")
-	);
-
 describe("Slash Commands", () => {
 	describe("SlashCommandBuilder", () => {
+		const createBuilder = () => new SlashCommandBuilder("test", "test description");
+		const createBuilderWithOptionalParams = () =>
+			new SlashCommandBuilder("test", "test description", {
+				nameLocalizations: {
+					de: "test",
+					"en-GB": "test"
+				},
+				descriptionLocalizations: {
+					de: "test description",
+					"en-GB": "test description"
+				},
+				dmPermission: true,
+				defaultMemberPermissions: 8
+			});
+		const createBuilderWithOptions = () =>
+			createBuilder()
+				.addOption(
+					new SlashCommandOptionBuilder(
+						"attachment",
+						ApplicationCommandOptionType.Attachment,
+						"attachment option"
+					)
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("boolean", ApplicationCommandOptionType.Boolean, "boolean option")
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("channel", ApplicationCommandOptionType.Channel, "channel option")
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("integer", ApplicationCommandOptionType.Integer, "integer option")
+				)
+				.addOption(
+					new SlashCommandOptionBuilder(
+						"mentionable",
+						ApplicationCommandOptionType.Mentionable,
+						"mentionable option"
+					)
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("number", ApplicationCommandOptionType.Number, "number option")
+				)
+				.addOption(new SlashCommandOptionBuilder("role", ApplicationCommandOptionType.Role, "role option"))
+				.addOption(
+					new SlashCommandOptionBuilder("string", ApplicationCommandOptionType.String, "string option")
+				)
+				.addOption(new SlashCommandOptionBuilder("user", ApplicationCommandOptionType.User, "user option"));
+		const createBuilderWithSubcommand = () =>
+			createBuilder().addSubcommand(new SlashCommandSubcommandBuilder("subcommand", "subcommand description"));
+		const createBuilderWithSubcommandGroup = () =>
+			createBuilder().addSubcommandGroup(
+				new SlashCommandSubcommandGroupBuilder("subcommandgroup", "subcommandgroup description")
+			);
+
 		describe("SlashCommandBuilder without options", () => {
 			test("GIVEN invalid builder THEN throw error", () => {
-				expect(() => createInvalidBuilder()).toThrowError();
+				expect(() => new SlashCommandBuilder("", "test description")).toThrowError();
+
+				expect(() => new SlashCommandBuilder("test", "")).toThrowError();
 			});
 
 			test("GIVEN valid builder THEN does not throw error", () => {
-				expect(() => createValidBuilder()).not.toThrowError();
+				expect(() => createBuilder()).not.toThrowError();
 
-				expect(() => createValidBuilder().toJSON()).not.toThrowError();
+				expect(() => createBuilder().toJSON()).not.toThrowError();
 			});
 
 			test("GIVEN valid builder with optional parameters THEN does not throw error", () => {
-				expect(() => createValidBuilderWithOptionalParams()).not.toThrowError();
+				expect(() => createBuilderWithOptionalParams()).not.toThrowError();
 
-				expect(() => createValidBuilderWithOptionalParams().toJSON()).not.toThrowError();
+				expect(() => createBuilderWithOptionalParams().toJSON()).not.toThrowError();
 			});
 		});
 
 		describe("SlashCommandBuilder with simple options", () => {
 			test("GIVEN valid builder with options THEN does not throw error", () => {
-				expect(() => createValidBuilderWithOptions()).not.toThrowError();
+				expect(() => createBuilderWithOptions()).not.toThrowError();
 
-				expect(() => createValidBuilderWithOptions().toJSON()).not.toThrowError();
+				expect(() => createBuilderWithOptions().toJSON()).not.toThrowError();
 			});
 		});
 
 		describe("SlashCommandBuilder with subcommands", () => {
 			test("GIVEN valid builder with subcommands THEN does not throw error", () => {
-				expect(() => createValidBuilderWithSubcommand()).not.toThrowError();
+				expect(() => createBuilderWithSubcommand()).not.toThrowError();
 
-				expect(() => createValidBuilderWithSubcommand().toJSON()).not.toThrowError();
+				expect(() => createBuilderWithSubcommand().toJSON()).not.toThrowError();
 			});
 		});
 
 		describe("SlashCommandBuilder with subcommand groups", () => {
 			test("GIVEN valid builder with subcommand groups THEN does not throw error", () => {
-				expect(() => createValidBuilderWithSubcommandGroup()).not.toThrowError();
+				expect(() => createBuilderWithSubcommandGroup()).not.toThrowError();
 
-				expect(() => createValidBuilderWithSubcommandGroup().toJSON()).not.toThrowError();
+				expect(() => createBuilderWithSubcommandGroup().toJSON()).not.toThrowError();
 			});
+		});
+	});
+
+	describe("SlashCommandSubcommandBuilder", () => {
+		const createBuilder = () => new SlashCommandSubcommandBuilder("test", "test description");
+		const createBuilderWithOptions = () =>
+			createBuilder()
+				.addOption(
+					new SlashCommandOptionBuilder(
+						"attachment",
+						ApplicationCommandOptionType.Attachment,
+						"attachment option"
+					)
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("boolean", ApplicationCommandOptionType.Boolean, "boolean option")
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("channel", ApplicationCommandOptionType.Channel, "channel option")
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("integer", ApplicationCommandOptionType.Integer, "integer option")
+				)
+				.addOption(
+					new SlashCommandOptionBuilder(
+						"mentionable",
+						ApplicationCommandOptionType.Mentionable,
+						"mentionable option"
+					)
+				)
+				.addOption(
+					new SlashCommandOptionBuilder("number", ApplicationCommandOptionType.Number, "number option")
+				)
+				.addOption(new SlashCommandOptionBuilder("role", ApplicationCommandOptionType.Role, "role option"))
+				.addOption(
+					new SlashCommandOptionBuilder("string", ApplicationCommandOptionType.String, "string option")
+				)
+				.addOption(new SlashCommandOptionBuilder("user", ApplicationCommandOptionType.User, "user option"));
+
+		describe("SlashCommandSubcommandBuilder without options", () => {
+			test("GIVEN invalid builder THEN throw error", () => {
+				expect(() => new SlashCommandSubcommandBuilder("", "test description")).toThrowError();
+
+				expect(() => new SlashCommandSubcommandBuilder("test", "")).toThrowError();
+			});
+
+			test("GIVEN valid builder THEN does not throw error", () => {
+				expect(() => createBuilder()).not.toThrowError();
+
+				expect(() => createBuilder().toJSON()).not.toThrowError();
+			});
+		});
+
+		describe("SlashCommandSubcommandBuilder with simple options", () => {
+			test("GIVEN valid builder with options THEN does not throw error", () => {
+				expect(() => createBuilderWithOptions()).not.toThrowError();
+
+				expect(() => createBuilderWithOptions().toJSON()).not.toThrowError();
+			});
+		});
+	});
+
+	describe("SlashCommandSubcommandGroupBuilder", () => {
+		const createBuilder = () => new SlashCommandSubcommandGroupBuilder("test", "test description");
+		const createBuilderWithSubcommand = () =>
+			createBuilder().addSubcommand(new SlashCommandSubcommandBuilder("test", "test description"));
+
+		describe("SlashCommandSubcommandGroupBuilder without subcommands", () => {
+			test("GIVEN invalid builder THEN throw error", () => {
+				expect(() => new SlashCommandSubcommandGroupBuilder("", "test description")).toThrowError();
+
+				expect(() => new SlashCommandSubcommandGroupBuilder("test", "")).toThrowError();
+			});
+
+			test("GIVEN valid builder THEN does not throw error", () => {
+				expect(() => createBuilder()).not.toThrowError();
+
+				expect(() => createBuilder().toJSON()).not.toThrowError();
+			});
+		});
+
+		describe("SlashCommandSubcommandGroupBuilder with subcommands", () => {
+			test("GIVEN valid builder with subcommands THEN does not throw error", () => {
+				expect(() => createBuilderWithSubcommand()).not.toThrowError();
+
+				expect(() => createBuilderWithSubcommand().toJSON()).not.toThrowError();
+			});
+		});
+	});
+
+	describe("SlashCommandOptionBuilder", () => {
+		const createBuilder = (type: SlashCommandOptionBuilder.Type) =>
+			new SlashCommandOptionBuilder("test", type, "test description");
+
+		test("GIVEN invalid builder THEN throw error", () => {
+			expect(
+				() =>
+					new SlashCommandOptionBuilder(
+						"test",
+						ApplicationCommandOptionType.Subcommand as SlashCommandOptionBuilder.Type,
+						"test"
+					)
+			).toThrowError();
+
+			expect(
+				() => new SlashCommandOptionBuilder("", ApplicationCommandOptionType.String, "test description")
+			).toThrowError();
+
+			expect(() => new SlashCommandOptionBuilder("test", ApplicationCommandOptionType.String, "")).toThrowError();
+		});
+
+		test("GIVEN valid builder THEN does not throw error", () => {
+			expect(() => createBuilder(ApplicationCommandOptionType.Attachment)).not.toThrowError();
+
+			expect(() => createBuilder(ApplicationCommandOptionType.Attachment).toJSON()).not.toThrowError();
 		});
 	});
 });
