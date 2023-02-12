8d097d8858997c6daa2a1f1d32212642313510aa
Jaron <jaron_zijlstra@outlook.com>
2023-02-12 15:11:07 +0100

    feat(Builders): add required option for BaseSlashCommandOptionBuilder

--- packages/builders/src/lib/slashCommand/options/BaseSlashCommandOptionBuilder.ts
+++ packages/builders/src/lib/slashCommand/options/BaseSlashCommandOptionBuilder.ts
@@ -5,6 +5,7 @@ export abstract class BaseSlashCommandOptionBuilder<Type extends SlashCommandOpt
 	readonly #type: Type;
 	readonly #name: string;
 	readonly #description: string;
+	readonly #required?: boolean;
 	readonly #name_localizations?: Partial<Record<LocaleString, string>> | null;
 	readonly #description_localizations?: Partial<Record<LocaleString, string>> | null;
 
@@ -17,6 +18,7 @@ export abstract class BaseSlashCommandOptionBuilder<Type extends SlashCommandOpt
 		this.#name = name;
 		this.#type = type;
 		this.#description = description;
+		this.#required = options?.required;
 		this.#name_localizations = options?.nameLocalizations;
 		this.#description_localizations = options?.descriptionLocalizations;
 	}
@@ -26,6 +28,7 @@ export abstract class BaseSlashCommandOptionBuilder<Type extends SlashCommandOpt
 			type: this.#type,
 			name: this.#name,
 			description: this.#description,
+			required: this.#required,
 			name_localizations: this.#name_localizations,
 			description_localizations: this.#description_localizations
 		};
@@ -34,6 +37,7 @@ export abstract class BaseSlashCommandOptionBuilder<Type extends SlashCommandOpt
 
 export namespace BaseSlashCommandOptionBuilder {
 	export interface Options {
+		required?: boolean;
 		nameLocalizations?: Partial<Record<LocaleString, string>> | null;
 		descriptionLocalizations?: Partial<Record<LocaleString, string>> | null;
 	}
